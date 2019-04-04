ada8a122345abae9b027b83d0fc99bb069f2eb65
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-04-05 00:28:26 +0600

    WordPress theme Development
    ১৮.২ - থিম সেটআপ  বুটস্ট্র্যাপিং

--- app/public/wp-content/themes/philosophy/functions.php
+++ app/public/wp-content/themes/philosophy/functions.php
@@ -1,3 +1,13 @@
 <?php
 
-add_action("after_setup_theme", "philosophy");
+
+function philosophy_theme_setup() {
+    load_theme_textdomain( "philosophy" );
+    add_theme_support("post-thumbnail");
+    add_theme_support("title-tag");
+    add_theme_support('html5', array('search-form', 'comment-list'));
+    add_theme_support("post-formats", array("image","gallery","quote","audio","video","link"));
+    add_theme_support();
+    add_theme_support();
+}
+add_action("after_setup_theme", "philosophy_theme_setup");
