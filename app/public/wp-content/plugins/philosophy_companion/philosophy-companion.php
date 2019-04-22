893beb360e1e07c26506f81b2dce5011751fb97a
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-04-23 00:48:08 +0600

    WordPress Theme Development
    ২১.৫ - কোড জেনারেশন টুলের সাহায্য নেয়া

--- app/public/wp-content/plugins/philosophy_companion/philosophy-companion.php
+++ app/public/wp-content/plugins/philosophy_companion/philosophy-companion.php
@@ -11,46 +11,61 @@
 	Version: 1.0
 */
 
-function philosophy_companion_register_my_cpts_book() {
 
-	/**
-	 * Post Type: Books.
-	 */
+// Register Custom Post Type Book
+function create_book_cpt() {
 
 	$labels = array(
-		"name" => __( "Books", "philosophy" ),
-		"singular_name" => __( "Book", "philosophy" ),
-		"all_items" => __( "My Books", "philosophy" ),
-		"add_new" => __( "New Book", "philosophy" ),
-		"featured_image" => __( "Book Cover", "philosophy" ),
+		'name' => _x( 'Books', 'Post Type General Name', 'philosophy' ),
+		'singular_name' => _x( 'Book', 'Post Type Singular Name', 'philosophy' ),
+		'menu_name' => _x( 'Books', 'Admin Menu text', 'philosophy' ),
+		'name_admin_bar' => _x( 'Book', 'Add New on Toolbar', 'philosophy' ),
+		'archives' => __( 'Book Archives', 'philosophy' ),
+		'attributes' => __( 'Book Attributes', 'philosophy' ),
+		'parent_item_colon' => __( 'Parent Book:', 'philosophy' ),
+		'all_items' => __( 'All Books', 'philosophy' ),
+		'add_new_item' => __( 'Add New Book', 'philosophy' ),
+		'add_new' => __( 'Add New', 'philosophy' ),
+		'new_item' => __( 'New Book', 'philosophy' ),
+		'edit_item' => __( 'Edit Book', 'philosophy' ),
+		'update_item' => __( 'Update Book', 'philosophy' ),
+		'view_item' => __( 'View Book', 'philosophy' ),
+		'view_items' => __( 'View Books', 'philosophy' ),
+		'search_items' => __( 'Search Book', 'philosophy' ),
+		'not_found' => __( 'Not found', 'philosophy' ),
+		'not_found_in_trash' => __( 'Not found in Trash', 'philosophy' ),
+		'featured_image' => __( 'Featured Image', 'philosophy' ),
+		'set_featured_image' => __( 'Set featured image', 'philosophy' ),
+		'remove_featured_image' => __( 'Remove featured image', 'philosophy' ),
+		'use_featured_image' => __( 'Use as featured image', 'philosophy' ),
+		'insert_into_item' => __( 'Insert into Book', 'philosophy' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Book', 'philosophy' ),
+		'items_list' => __( 'Books list', 'philosophy' ),
+		'items_list_navigation' => __( 'Books list navigation', 'philosophy' ),
+		'filter_items_list' => __( 'Filter Books list', 'philosophy' ),
 	);
-
 	$args = array(
-		"label" => __( "Books", "philosophy" ),
-		"labels" => $labels,
-		"description" => "",
-		"public" => true,
-		"publicly_queryable" => true,
-		"show_ui" => true,
-		"delete_with_user" => false,
-		"show_in_rest" => true,
-		"rest_base" => "",
-		"rest_controller_class" => "WP_REST_Posts_Controller",
-		"has_archive" => false,
-		"show_in_menu" => true,
-		"show_in_nav_menus" => true,
-		"exclude_from_search" => false,
-		"capability_type" => "post",
-		"map_meta_cap" => true,
-		"hierarchical" => false,
-		"rewrite" => array( "slug" => "book", "with_front" => true ),
-		"query_var" => true,
-		"menu_icon" => "dashicons-book",
-		"supports" => array( "title", "editor", "thumbnail", "excerpt", "author" ),
-		"taxonomies" => array( "category", "post_tag" ),
+		'label' => __( 'Book', 'philosophy' ),
+		'description' => __( '', 'philosophy' ),
+		'labels' => $labels,
+		'menu_icon' => 'dashicons-email-alt',
+		'supports' => array('title', 'editor', 'excerpt', 'thumbnail'),
+		'taxonomies' => array(),
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'menu_position' => 5,
+		'show_in_admin_bar' => true,
+		'show_in_nav_menus' => true,
+		'can_export' => true,
+		'has_archive' => true,
+		'hierarchical' => false,
+		'exclude_from_search' => false,
+		'show_in_rest' => true,
+		'publicly_queryable' => true,
+		'capability_type' => 'post',
 	);
+	register_post_type( 'book', $args );
 
-	register_post_type( "book", $args );
 }
-
-add_action( 'init', 'philosophy_companion_register_my_cpts_book' );
+add_action( 'init', 'create_book_cpt', 0 );
