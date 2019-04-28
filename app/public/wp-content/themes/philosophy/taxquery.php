1181d25224fbbb31959448af3bd135b605b3e65e
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-04-28 11:26:56 +0600

    WordPress Theme Development
    ২২.৩ - ট্যাক্স কোয়েরীর সাথে পরিচয়

---
+++ app/public/wp-content/themes/philosophy/taxquery.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+Template Name: Tax Query Example
+
+*/
+
+$philosophy_query_arg = array(
+	'post_type' => 'book',
+	'posts_per_page' =>-1,
+	'tax_query' => array(
+		array(
+			'taxonomy' => 'language',
+			'field' => 'slug',
+			'terms' => array('english','bangla')
+		)
+	),
+);
+
+$philosophy_posts = new WP_Query($philosophy_query_arg);
+
+// echo $philosophy_posts->found_posts;
+
+while ($philosophy_posts->have_posts()) {
+	$philosophy_posts->the_post();
+	the_title();
+	echo "<br/>";
+}
+
+wp_reset_query();
