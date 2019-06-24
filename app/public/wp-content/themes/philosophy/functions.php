ce420c83f660a61b89c95cb54b26e132dc7c341e
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-06-25 01:19:20 +0600

    ২৬.১ - অ্যাজাক্স কি এবং কেন - জাভাস্ক্রিপ্টের পার্ট ২৬.২ - ওয়ার্ডপ্রেসের ব্যাকএন্ডে অ্যাজাক্স কল প্রসেস করা (লগডইন এবং নট লগডইন ইউজারদের জন্য) ২৬.৩ - অ্যাজাক্স কলগুলোর জন্য ওয়ার্ডপ্রেস নন্স এবং সিকিউরিটি
    ২৬.১ - অ্যাজাক্স কি এবং কেন - জাভাস্ক্রিপ্টের পার্ট
    ২৬.২ - ওয়ার্ডপ্রেসের ব্যাকএন্ডে অ্যাজাক্স কল প্রসেস করা (লগডইন এবং নট লগডইন ইউজারদের জন্য)
    ২৬.৩ - অ্যাজাক্স কলগুলোর জন্য ওয়ার্ডপ্রেস নন্স এবং সিকিউরিটি

--- app/public/wp-content/themes/philosophy/functions.php
+++ app/public/wp-content/themes/philosophy/functions.php
@@ -28,7 +28,6 @@ function philosophy_theme_setup() {
     add_theme_support("custom-logo");
     add_theme_support('html5', array('search-form', 'comment-list'));
     add_theme_support("post-formats", array("image","gallery","quote","audio","video","link"));
-    add_theme_support( 'automatic-feed-links' );
     add_editor_style( "/assets/css/editor-style.css" );
 
 
@@ -69,6 +68,14 @@ function philosophy_assets() {
     if ( is_singular() ) {
         wp_enqueue_script( "comment-reply" );
     }
+
+    if (is_page_template( 'ajax.php' )) {
+        wp_enqueue_script("ajaxtest-js",get_theme_file_uri("/assets/js/ajaxtest.js"),array("jquery"),time(),true);
+        $ajaxurl = admin_url('admin-ajax.php');
+
+        wp_localize_script( 'ajaxtest-js', 'urls', array('ajaxurl'=>$ajaxurl) );
+    }
+        
     wp_enqueue_script("main-js",get_theme_file_uri("/assets/js/main.js"),array("jquery"),"1.0",true);
 }
 add_action("wp_enqueue_scripts", "philosophy_assets");
@@ -320,3 +327,33 @@ function philosophy_footer_language_terms($tags) {
     return $tags;
 }
 add_filter( 'philosophy_footer_tag_items', 'philosophy_footer_language_terms' );
+
+
+
+// AJAX FUNCTION
+function philisophy_ajaxtest(){
+    if (check_ajax_referer('ajaxtest','s')) {
+        $info = $_POST['info'];
+        echo strtoupper($info);
+
+        die();        
+    }
+}
+add_action('wp_ajax_ajaxtest','philisophy_ajaxtest');
+
+function philisophy_ajaxtest_nopriv(){
+     if (check_ajax_referer('ajaxtest','s')) {
+        $info = $_POST['info'];
+        echo strtoupper("GLOBAL ".$info);
+
+        die();       
+    }
+}
+add_action('wp_ajax_nopriv_ajaxtest','philisophy_ajaxtest_nopriv');
+
+
+
+function philosophy_nonce_life(){
+    return 20;
+}
+add_filter('nonce_life','philosophy_nonce_life');
