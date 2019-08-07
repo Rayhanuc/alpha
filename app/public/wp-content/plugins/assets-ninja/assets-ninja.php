f7480d4d1e1edbbdb75bc6d20cb3a8ba3b9450f2
[Rayanuc] <[rayhanctg4@gmail.com]>
2019-08-07 20:16:41 +0600

    ১০.২ - অ্যাসেটের ডিপেন্ডেন্সি ম্যানেজমেন্ট
    ১০.২ - অ্যাসেটের ডিপেন্ডেন্সি ম্যানেজমেন্ট

--- app/public/wp-content/plugins/assets-ninja/assets-ninja.php
+++ app/public/wp-content/plugins/assets-ninja/assets-ninja.php
@@ -11,9 +11,20 @@ Text Domain: assetsninja
 Domain Path: /languages/
  */
 
+// নির্দিষ্ট একটা অংশ একই রকম থাকলে এগুলিকে ডিফাইন করে নিলে বার বার লিখতে হয় না।
+define("ASN_ASSETS_DIR",plugin_dir_url(__FILE__)."assets/");
+define("ASN_ASSETS_PUBLIC_DIR",plugin_dir_url(__FILE__)."assets/public");
+define("ASN_ASSETS_ADMIN_DIR",plugin_dir_url(__FILE__)."assets/admin");
+define("ASN_VERSION", time());
+
 
 Class AssetsNinja{
+
+	private $version;
+
 	function __construct() {
+
+		$this->version = time();
 
 		add_action( 'plugins_loaded', array($this,'load_textdomain') );
 		add_action('wp_enqueue_scripts',array($this,'load_front_assets'));
@@ -25,6 +36,17 @@ Class AssetsNinja{
 	}
 
 	function load_front_assets(){
+		wp_enqueue_style( 'asn-main-css',ASN_ASSETS_PUBLIC_DIR."/css/main.css",null,$this->version );
+
+
+
+		wp_enqueue_script('asn-main-js',ASN_ASSETS_PUBLIC_DIR."/js/main.js",array(
+			'jquery',
+			'asn-another-js'
+		),$this->version,true);
+
+		wp_enqueue_script('asn-another-js',ASN_ASSETS_PUBLIC_DIR."/js/another.js",array('jquery','asn-more-js'),$this->version,true);
+		wp_enqueue_script('asn-more-js',ASN_ASSETS_PUBLIC_DIR."/js/more.js",array('jquery'),$this->version,true);
 
 	}
 }
---
+++ app/public/wp-content/plugins/assets-ninja/assets/public/js/main.js
@@ -0,0 +1,5 @@
+;(function($){
+	$(document).ready(function(){
+		alert('Hello I am in assets-ninja>assets>public>main.js');
+	});
+})(jQuery);
