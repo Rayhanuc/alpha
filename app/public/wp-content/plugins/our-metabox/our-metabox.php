5e49bc6e03b1fe07f73ba20aa44c291868a65479
[Rayanuc] <[rayhanctg4@gmail.com]>
2019-08-15 23:35:00 +0600

    ১১.৫ - মেটাবক্সে মাল্টিপল/গ্রুপড চেকবক্স ফিল্ড নিয়ে কাজ করা
    ১১.৫ - মেটাবক্সে মাল্টিপল/গ্রুপড চেকবক্স ফিল্ড নিয়ে কাজ করা

--- app/public/wp-content/plugins/our-metabox/our-metabox.php
+++ app/public/wp-content/plugins/our-metabox/our-metabox.php
@@ -58,6 +58,7 @@ class OurMetabox{
 		$location = isset($_POST['omb_location'])?$_POST['omb_location'] : '';
 		$country = isset($_POST['omb_country'])?$_POST['omb_country'] : '';
 		$is_favorite = isset($_POST['omb_is_favorite'])?$_POST['omb_is_favorite'] : 0;
+		$colors = isset($_POST['omb_clr'])?$_POST['omb_clr'] : array();
 
 		if ($location == '' || $country == '') {
 			return $post_id;
@@ -70,6 +71,7 @@ class OurMetabox{
 		update_post_meta($post_id,'omb_location',$location);
 		update_post_meta($post_id,'omb_country',$country);
 		update_post_meta($post_id,'omb_is_favorite',$is_favorite);
+		update_post_meta($post_id,'omb_clr',$colors);
 	}
 
 	function omb_add_metabox() {
@@ -86,15 +88,17 @@ class OurMetabox{
 	function omb_display_metabox($post) {
 		$location = get_post_meta($post->ID,'omb_location',true);
 		$country = get_post_meta($post->ID,'omb_country',true);
-		$is_favorite = get_post_meta($post->ID,'omb_is_favorite',true);
-		echo "Is Fav:".$is_favorite."<br/>";
-
-
+		$is_favorite = get_post_meta('omb_is_favorite',true);
 		$checked = $is_favorite==1?'checked':'';
+
+		$saved_colors = get_post_meta($post->ID,'omb_clr',true);
+
+
 
 		$label1 = __('Location','our-metabox');
 		$label2 = __('Country','our-metabox');
 		$label3 = __('Is Favorite','our-metabox');
+		$label4 = __('Colors','our-metabox');
 
 		$colors = array('red','green','blue','yellow','magenta','pink','black');
 
@@ -113,7 +117,22 @@ class OurMetabox{
 <label for="omb_is_favorite">{$label3}: </label>
 <input type="checkbox" name="omb_is_favorite" id="omb_is_favorite" value="1" {$checked}/>
 </p>
+
+<p>
+<label for="omb_is_favorite">{$label4}: </label>
 EOD;
+
+		foreach ($colors as $color) {
+			$_color = ucwords($color);
+			$checked = in_array($color, $saved_colors)?'checked':'';
+			$metabox_html .= <<<EOD
+<label for="omb_clr_{$color}">{$_color}</label>
+<input type="checkbox" name="omb_clr[]" id="omb_clr_{$color}" value="{$color}" {$checked}/>
+
+EOD;
+		}
+		
+		$metabox_html .= "</p>";
 
 		echo $metabox_html;
 	}
